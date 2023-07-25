3ade39ab0493f87aecbd64fc34753bfa8c2a4a79
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2023-07-25 23:30:35 +0530

    Initialize

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class=" text-black">
+        Error: <span class=" text-red-500">{{ route.statusCode }}</span>
+    </h1>
+    <p>
+        {{ route.statusMessage }}
+    </p>
+</template>
+
+<script setup>
+const route = useError()
+</script>
