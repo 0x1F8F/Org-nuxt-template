03960cd414a826d083e4db0b2f9ebb921c8980a3
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2023-07-28 20:56:05 +0530

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Header</h1>
+  <Header/>
   <div>
     <NuxtPage/>
   </div>
---
+++ components/header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-row justify-between p-4">
+        <div>
+            <h1 class=" text-2rem font-extrabold text-black">
+                A<span class=" text-purple-400 font-mono">j</span>
+            </h1>
+        </div>
+        <div class=" flex flex-row">
+            <ul><h2 class=" hover:underline px-2">Home</h2></ul>
+            <ul><h2 class=" hover:underline px-2">about</h2></ul>
+        </div>
+        <div>
+            <button>Sign up</button>
+            <button class="button">Login</button>
+        </div>
+    </div>
+</template>
+
+<style>
+.button {
+    @apply p-2 bg-purple-500 rounded-full;
+}
+</style>
