035349102ceb236ec2faa8ee21bbfd1e9a114bef
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2023-07-29 15:10:54 +0530

    update

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -5,12 +5,17 @@
                 A<span class=" text-purple-400 font-mono">j</span>
             </h1>
         </div>
-        <div class=" flex flex-row">
-            <ul><h2 class=" hover:underline px-2">Home</h2></ul>
-            <ul><h2 class=" hover:underline px-2">about</h2></ul>
+        <div class=" flex flex-row text-gray-600">
+            <NuxtLink to="/">
+                <ul><h2 class=" hover:underline px-2">Home</h2></ul>
+            </NuxtLink>
+            <NuxtLink to="/about">
+                <ul><h2 class=" hover:underline px-2">about</h2></ul>
+            </NuxtLink>
+            <ul><h2>{{ $route.path=="/"? "root":"about" }}</h2></ul>
         </div>
         <div>
-            <button>Sign up</button>
+            <button class="button">Sign up</button>
             <button class="button">Login</button>
         </div>
     </div>
@@ -18,6 +23,10 @@
 
 <style>
 .button {
-    @apply p-2 bg-purple-500 rounded-full;
+    @apply p-2 bg-purple-500 rounded-full mx-1 text-sm;
 }
 </style>
+
+<script setup>
+ 
+</script>
