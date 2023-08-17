4ad15ea94f10a9c265d9de7341cd50c5571938c9
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2023-08-17 20:30:54 +0530

    update

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -3,11 +3,14 @@
         <NuxtLink to="/">
             <h5>Home</h5>
         </NuxtLink>
-        <form @submit.prevent="login">
-            <input type="text">
-            <input type="password">
-            <input type="submit" value="Login">
-        </form>
+        <div class="flex flex-col items-center h-screen justify-center">
+            <form @submit.prevent="login" class="flex flex-col items-center m-2">
+                <input type="text" placeholder="Username or Email" class="inputField">
+                <input type="password" placeholder="Password" class="inputField">
+                <input type="submit" value="Login" class=" bg-black text-white rounded-md shadow-md px-3 py-2" >
+            </form>
+        </div>
+        
     </div>
 </template>
 
@@ -16,3 +19,9 @@ definePageMeta({
   layout: false,
 });
 </script>
+
+<style>
+.inputField {
+    @apply m-2 placeholder:p-2 font-sans text-xl
+}
+</style>
